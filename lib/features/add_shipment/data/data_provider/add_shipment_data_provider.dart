0aae0ab22a6c1354cbd43d68bc7af6bb9b5ce0b8
AbdulsemedM <abdumussema22@gmail.com>
2025-03-12 12:20:43 +0300

    add shipment data provider added

---
+++ lib/features/add_shipment/data/data_provider/add_shipment_data_provider.dart
@@ -0,0 +1,90 @@
+import 'package:courier_app/configuration/api_constants.dart';
+import 'package:courier_app/providers/provider_setup.dart';
+
+class AddShipmentDataProvider {
+  Future<String> fetchBranches() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/branches");
+      // print(response.body);
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchDeliveryTypes() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/delivery-types");
+      // print(response.body);
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchPaymentMethods() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/payment-methods");
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchPaymentModes() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/payment-modes");
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchServiceModes() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/service-modes");
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchShipmentTypes() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/shipment-types");
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  Future<String> fetchTransportModes() async {
+    try {
+      final apiProvider = ProviderSetup.getApiProvider(ApiConstants.baseUrl);
+      final response = await apiProvider.getRequest("/api/v1/transport-modes");
+      return response.body;
+    } catch (e) {
+      // print("here is the response");
+      // print(e.toString());
+      throw e.toString();
+    }
+  }
+}
